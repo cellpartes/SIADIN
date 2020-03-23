766a882971c9b523182e452b949e8ca0a067a95f
roger <cellpartes.aqp@gmail.com>
2020-03-23 11:37:05 -0500

    conexion en la nube

--- CapaDatos/cdConexion.vb
+++ CapaDatos/cdConexion.vb
@@ -10,11 +10,11 @@ Public Class cdConexion
         conexion = New MySqlConnection
         Dim myConnectionString As String
         'Desa
-        myConnectionString = "server=192.168.0.64;Port=3306;Database=desarrollo;Uid=useradd;Pwd=$Lenovo01;"
+        'myConnectionString = "server=192.168.0.64;Port=3306;Database=desarrollo;Uid=useradd;Pwd=$Lenovo01;"
         'Prod Localhost
         'myConnectionString = "server=192.168.0.64;Port=3306;Database=imexgonp;Uid=useradd;Pwd=$Lenovo01;"
         'Prod Web
-        'myConnectionString = "server=167.114.163.236;Port=3306;Database=imexgonp_basedatos;Uid=imexgonp_useradd;Pwd=$Lenovo01;"
+        myConnectionString = "server=167.114.163.236;Port=3306;Database=imexgonp_basedatos;Uid=imexgonp_useradd;Pwd=$Lenovo01;"
         Try
             conexion.ConnectionString = myConnectionString
             'conexion.Open()
